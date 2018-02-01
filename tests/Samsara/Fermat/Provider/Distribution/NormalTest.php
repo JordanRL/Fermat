d59e87f78041db7a45199ebe74f55593205198f2
JordanRL <jordan.ledoux@gmail.com>
2018-02-01 02:25:37 -0800

    Normal distribution tests

---
+++ tests/Samsara/Fermat/Provider/Distribution/NormalTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Samsara\Fermat\Provider\Distribution;
+
+use PHPUnit\Framework\TestCase;
+
+class NormalTest extends TestCase
+{
+
+    public function testPercentAboveX()
+    {
+
+        $normal = new Normal(10, 2);
+
+        $this->assertEquals('0.8413447461', $normal->percentAboveX(8)->getValue());
+
+        $this->assertEquals('0.1498822848', $normal->pdf(8, 9)->getValue());
+
+    }
+
+    public function testCDF()
+    {
+
+        $normal = new Normal(10, 2);
+
+        $this->assertEquals('0.1586552539', $normal->cdf(8)->getValue());
+
+    }
+
+    public function testZScore()
+    {
+
+        $normal = new Normal(10, 2);
+
+        $this->assertEquals('-1', $normal->zScoreOfX(8)->getValue());
+
+        $this->assertEquals('12', $normal->xFromZScore(1)->getValue());
+
+    }
+
+}
